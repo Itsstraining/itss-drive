ebca627c97b8158c3064e0237816dc72f6e3c26f
Tin <tin.td08891@sinhvien.hoasen.edu.vn>
2022-08-20 16:53:55 +0700

    doan tin

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'main', loadChildren: () => import('./main/main/main.module').then(m => m.MainModule) }];
+const routes: Routes = [
+  { path: 'main', loadChildren: () => import('../app/pages/main/main.module').then(m => m.MainModule) }, 
+  { path: 'login', loadChildren: () => import('./login-page/login/login.module').then(m => m.LoginModule) }, 
+  { path: 'inro', loadChildren: () => import('./pages/inro/inro.module').then(m => m.InroModule) },
+  { path: 'SignUp', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
