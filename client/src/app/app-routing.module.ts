5d704f885f013c45b7a7b599045bd1936eaf5a6b
trinhac <cuong.ta14584@sinhvien.hoasen.edu.vn>
2022-08-22 18:10:49 +0700

    Web22A August 22 - Google Clone Project(1): UI with Create and Rename folder button, don't have folder yet

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'main', loadChildren: () => import('./main/main/main.module').then(m => m.MainModule) }];
+const routes: Routes = [
+  { path: 'my-drive', 
+    loadChildren: () => 
+    import('./pages/main/main.module').then(m => m.MainModule) 
+  },
+  { path: 'recent', loadChildren: () => import('./pages/recent/recent.module').then(m => m.RecentModule) },
+  { path: 'favorites', loadChildren: () => import('./pages/favorites/favorites.module').then(m => m.FavoritesModule) },
+  { path: 'trash', loadChildren: () => import('./pages/trash/trash.module').then(m => m.TrashModule) },
+
+
+
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
