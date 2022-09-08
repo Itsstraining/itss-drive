74ad37e3139f2b43bdbba8f6e2a448ffa2f08b24
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-09-08 12:56:42 +0700

    fix login ui

--- client/src/app/login-page/login/login.component.ts
+++ client/src/app/login-page/login/login.component.ts
@@ -7,6 +7,7 @@ import { Subscription } from 'rxjs/internal/Subscription';
 import { Observable } from 'rxjs/internal/Observable';
 import { EMPTY } from 'rxjs/internal/observable/empty';
 import { AuthService } from 'src/app/services/auth.service';
+import { HotToastService } from '@ngneat/hot-toast';
 
 
 @Component({
@@ -22,20 +23,18 @@ export class LoginComponent implements OnInit {
     // Email : new FormControl('', [Validators.required,Validators.email]),
 //  })
   // email:string;
-  username: string;
-  password: string;
   // Router: any; 
   public form!: FormGroup
   
  
-  constructor(private auth: AuthService, private router: Router, private formBuilder: FormBuilder, private http: HttpClient   ) { 
+  constructor(private auth: AuthService, private router: Router, private formBuilder: FormBuilder, private http: HttpClient, private toast: HotToastService) { 
       
   }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      emai:[''],
-      password:[''],
+      email:['', [Validators.required, Validators.email]],
+      password:['', Validators.required],
     })
   }
 
@@ -57,76 +56,77 @@ export class LoginComponent implements OnInit {
   //     this.userDisposable.unsubscribe();
   //   }
   // }
-  login(){
-    if(this.username == ''){
-      alert('Please enter username');
-      return;
-    }
+  // login(){
+  //   if(this.username == ''){
+  //     alert('Please enter username');
+  //     return;
+  //   }
 
-    if(this.password == ''){
-      alert('Please enter password');
-    }
+  //   if(this.password == ''){
+  //     alert('Please enter password');
+  //   }
 
-    this.auth.login(this.username,this.password);
-    this.username;
-    this.password;
-    // this.email;
+  //   this.auth.login(this.username,this.password);
+  //   this.username;
+  //   this.password;
+  //   // this.email;
 
-    this.http.get<any>("http://localhost:4200/main")
-    .subscribe((res: any[])=>{
-      const user = res.find((a:any)=> {
-        return a.email = this.form.value.email && a.password == this.form.value.password
-      });
-      if(user){
-        alert("Login success");
-        this.form.reset();
-        this.router.navigate(['/main']);
-      }else{
-        alert('user not found');
-      }(error: any) =>{
-        alert("Something went wrong");
-      }
-    })
+  //   this.http.get<any>("http://localhost:4200/main")
+  //   .subscribe((res: any[])=>{
+  //     const user = res.find((a:any)=> {
+  //       return a.email = this.form.value.email && a.password == this.form.value.password
+  //     });
+  //     if(user){
+  //       alert("Login success");
+  //       this.form.reset();
+  //       this.router.navigate(['/main']);
+  //     }else{
+  //       alert('user not found');
+  //     }(error: any) =>{
+  //       alert("Something went wrong");
+  //     }
+  //   })
+  // }
+
+  get email(){
+    return this.form.get('email');
   }
 
-  // get email(){
-  //   return this.form.get('email');
-  // }
+  get username(){
+    return this.form.get('username');
+  }
 
-  // get username(){
-  //   return this.form.username('username');
-  // }
-
-  // get password(){
-  //   return this.form.password('password');
-  // }
+  get password(){
+    return this.form.get('password');
+  }
 
   
   SignInWithGoogle(): void{
     this.auth.SignInWithGoogle();
   }
 
+
  
 
-  // submit() {
-  //   const { username, password} = this.form.value;
+  submit() {
+    const { email, password} = this.form.value;
 
-  //   if (!this.form.valid || !username || !password) {
-  //     return;
-  //   }
+    if (!this.form.valid || !email || !password) {
+      return this.toast.error('Please fill all the fields');
+    }
 
-  //   this.auth
-  //     .login(username, password).pipe(
-  //       this.toast.observe({
-  //         success: 'Logged in successfully',
-  //         loading: 'Logging in...',
-  //         error: ({ message }) => `There was an error: ${message} `,
-  //       })
-  //     )
-  //     .subscribe(() => {
-  //       this.router.navigate(['/main']);
-  //     });
-  // }
+    return this.auth
+      .login(email, password).pipe(
+        this.toast.observe({
+          success: 'Logged in successfully',
+          loading: 'Logging in...',
+          error: ({ message }) => `There was an error: ${message} `,
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(['/main']);
+      });
+  }
 
 
 }
