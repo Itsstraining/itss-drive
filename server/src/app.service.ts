214f5c65d87161e1ebe2948624b09dbb8b615c1d
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-09-08 21:47:42 +0700

    fix login and add server

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, Req, Res } from '@nestjs/common';
+import { async } from 'rxjs';
+import { favorite } from './favorite.dto';
+import { firestore } from 'firebase-admin';
 
 @Injectable()
 export class AppService {
+  // favorite = []
+  fs = firestore().collection('element');
   getHello(): string {
     return 'Hello World!';
+  }
+  async getByID(id){
+    let result = (await this.fs.doc(id).get()).data();
+    return result;
+  }
+
+  async getFavorite(){
+    let docs = await this.fs.get();
+    // let res = docs.docs.map((data)=>data.data());
+    // return res;
+    let data = [];
+    docs.forEach((doc)=>{
+      data.push(doc.data());
+    }
+    )
+    return data;
+  }
+
+  // post file
+  CreateFavorite(@Body() favorite: favorite, @Req() req, @Res() res) {
+    // console.log(favorite);
+    // this.favorite.push(favorite);
+    // res.send('ok');
+    this.fs.add(favorite);
+    res.send('ok');
   }
 }
---
+++ server/src/favorite.dto.ts
@@ -0,0 +1,4 @@
+export class favorite{
+    id: string;
+    name: string;
+}
